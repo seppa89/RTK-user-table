199aaf0133cb5da4feea77d0f61a912de098f67f
Darek <darek0511@gmail.com>
2024-09-15 00:23:23 +0200

    style: add styling for table component

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from '../../store/store';
+import classes from './Table.module.css';
 
 const columns: ColumnsName[] = ['name', 'username', 'email', 'phone'];
 
@@ -6,15 +7,15 @@ export default function Table() {
 	const users = useAppSelector(store => store.users.users);
 
 	return (
-		<table>
-			<thead>
+		<table className={classes.table}>
+			<thead className={classes.head}>
 				<tr>
 					{columns.map(col => (
 						<th key={col}>{col}</th>
 					))}
 				</tr>
 			</thead>
-			<tbody>
+			<tbody className={classes.body}>
 				{users?.map(user => (
 					<tr key={user.id}>
 						<td>{user.name}</td>
